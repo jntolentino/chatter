ba16dfe42a23f21d69251b60e9917b472a27b4f6
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-08-13 00:38:31 +0800

    implement signup functionality with password validation in auth controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,12 @@
 export const signup = (req, res) => {
-  res.send("Signup route");
+  const { fullName, email, password } = req.body;
+  try {
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long." });
+    }
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
