0ba7b80cd933e8b09c8db2bf92659bf04ca37abc
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-08-13 00:38:38 +0800

    refactor signup controller to include user creation and token generation logic

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,14 @@
+export const generateToken = (userId, rese) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    sameSite: "strict",
+    secure: process.env.NODE_ENV != "development", // true in production
+  });
+
+  return token;
+};
