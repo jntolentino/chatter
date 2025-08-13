43ca4d431572449e65d13e5f676afe7805ef1998
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-08-14 00:31:46 +0800

    feat: add updateProfile functionality with Cloudinary integration for profile picture uploads

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  updateProfile,
+} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
