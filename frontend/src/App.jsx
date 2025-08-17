c9d0efe54372641e71c2eb3a44c2e9ae3a47882f
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-08-17 21:39:43 +0800

    feat: implement theme selection functionality with multiple themes and persistent storage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,15 @@ import ProfilePage from "./pages/ProfilePage";
 
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import { useThemeStore } from "./store/useThemeStore";
 
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -30,7 +33,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
