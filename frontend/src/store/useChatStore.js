c78836d70e0b904a0c2491f95856612124bc15ea
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-09-01 01:04:11 +0800

    feat: enhance Sidebar component to display online users and user status

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -12,7 +12,7 @@ export const useChatStore = create((set) => ({
   getUsers: async () => {
     set({ isUsersLoading: true });
     try {
-      const res = await axiosInstance.get("/messages/users");
+      const res = await axiosInstance.get("/messages/user");
       set({ users: res.data });
     } catch (error) {
       toast.error(error.response.data.messages);
