7f4e19adf7add7fd2fcf49fed830f31850378177
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-09-06 22:56:18 +0800

    feat: implement MessageInput component with image upload and message sending functionality

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import { useState } from "react";
+import { sendMessage } from "./../../../backend/src/controllers/message.controller";
+import { X } from "lucide-react";
+import { Image } from "lucide-react";
+import { useRef } from "react";
+import { useChatStore } from "../store/useChatStore";
+import { Send } from "lucide-react";
 
 const MessageInput = () => {
-  return <div>MessageInput</div>;
+  const [text, setText] = useState("");
+  const [imagePreview, setImagePreview] = useState(null);
+  const fileInputRef = useRef(null);
+  const { sendMessage } = useChatStore();
+
+  const handleImageChange = (e) => {};
+
+  const removeImage = () => {};
+
+  const handleSendMessage = async (e) => {};
+
+  return (
+    <div className="p-4 w-full">
+      {imagePreview && (
+        <div className="mb-3 flex items-center gap-2">
+          <div className="relative">
+            <img
+              src={imagePreview}
+              alt="Preview"
+              className="w-20 h-20 object-cover rounded-lg border border-zinc-700"
+            />
+            <button
+              onClick={removeImage}
+              className="absolute -top-1.5 -right-1.5 w-5 h-5 rounded-full bg-base-300
+              flex items-center justify-center"
+              type="button"
+            >
+              <X className="w-3 h-3" />
+            </button>
+          </div>
+        </div>
+      )}
+
+      <form onSubmit={handleSendMessage} className="flex items-center gap-2">
+        <div className="flex-1 flex gap-2 items-center">
+          <input
+            type="text"
+            className="w-full input input-bordered rounded-lg btn-sm"
+            placeholder="Type a message..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={fileInputRef}
+            className="hidden"
+          />
+
+          <button
+            type="button"
+            className={`btn btn-circle btn-sm flex items-center justify-center ${
+              imagePreview ? "text-emerald-500" : "text-zinc-400"
+            }`}
+            onClick={() => fileInputRef.current.click()}
+          >
+            <Image size={20} />
+          </button>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-circle btn-sm flex items-center justify-center"
+          disabled={!text.trim() && !imagePreview}
+        >
+          <Send size={20} />
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default MessageInput;
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "./../lib/axios";
+import { sendMessage } from "./../../../backend/src/controllers/message.controller";
 
-export const useChatStore = create((set) => ({
+export const useChatStore = create((set, get) => ({
   messages: [],
   users: [],
   selectedUser: null,
@@ -30,6 +31,19 @@ export const useChatStore = create((set) => ({
       toast.error(error.response.data.messages);
     } finally {
       set({ isMessagesLoading: false });
+    }
+  },
+
+  sendMessage: async (messageData) => {
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser.id} `,
+        messageData
+      );
+      set({ messages: [...messages, res.data] });
+    } catch (error) {
+      toast.error(error.response.data.messages);
     }
   },
 
