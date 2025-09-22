6362111103360bfd57f292232bd843b6c4044cf9
Jaime Nicolas Tolentino <jaimenicolastoletino@gmail.com>
2025-09-22 17:28:40 +0800

    feat: update server configuration for production; enhance axios and useAuthStore for dynamic base URL based on environment

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
@@ -12,6 +14,7 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 const PORT = process.env.PORT;
+const _dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
@@ -27,6 +30,14 @@ app.use(
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "..frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(
